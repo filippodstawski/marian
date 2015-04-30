61e7b573ea599e6b984d87dd14f27e2ee57fe9b9
Filip Podstawski <fp@katotec.pl>
2015-04-30 14:29:14 +0200

    add hit

--- maze.js
+++ maze.js
@@ -5,8 +5,11 @@ $(function(){
 			animationImg:"stay",
 			personDiv: 'player',
 			animationTime:0,
+			typeAction: 0,
 			positionX: 0,
 			velocity: 6,
+			timeHit: 0,
+			timeHitScene: 0,
 			positionY: 20,
 			getAnimationTime: function() {return this.animationTime;},
 			updateAnimationTime: function() {
@@ -45,10 +48,39 @@ $(function(){
 						this.animationImg="run1";
 					}
 				},
+			modifyHitStatus: function(){
+				this.timeHit++;
+				if(this.timeHit>5){
+					this.timeHit=0;
+					this.timeHitScene++;
+				}
+					if(this.timeHitScene==0){
+						this.animationImg="w1";
+					}
+					if(this.timeHitScene==1){
+						this.animationImg="w2";
+					}
+					if(this.timeHitScene==2){
+						this.animationImg="w3";
+					}
+					if(this.timeHitScene==3){
+						this.timeHit=0;
+						this.timeHitScene=0;
+						this.typeAction=0;
+					}
+			},
 			restoreAnimationTime: function() {this.animationTime = 0;},
 			modifyAnimation: function() {
-					this.updateAnimationImg();
-					$("#"+this.personDiv).css('background-image','url(player/'+this.animationImg+'.png)');
+					if(this.typeAction==0)
+					{
+						this.updateAnimationImg();
+						$("#"+this.personDiv).css('background-image','url(player/'+this.animationImg+'.png)');
+					}
+					if(this.typeAction==1)
+					{
+						this.modifyHitStatus();
+						$("#"+this.personDiv).css('background-image','url(player/'+this.animationImg+'.png)');
+					}
 				}
 		};
 		
@@ -63,37 +95,46 @@ $(function(){
 	$("#player").offset({ top: person.positionX, left: person.positionY});
 	
 	$(document).keydown(function(e){
-		
-		person.updateAnimationTime();
-		
-		  switch (e.keyCode) {
-			case 37: // Left
-				person.positionY = person.positionY - person.velocity;
-				$("#player").offset({ top: person.positionX, left: person.positionY});
-				//$("#player").css('left', position.left - 20 + 'px');
-				//Game.player.moveLeft();
-				break;
+			
+			if(person.typeAction==0){
+				
+			  switch (e.keyCode) {
+					case 37: // Left
+						person.updateAnimationTime();
+						person.positionY = person.positionY - person.velocity;
+						$("#player").offset({ top: person.positionX, left: person.positionY});
+						//$("#player").css('left', position.left - 20 + 'px');
+						//Game.player.moveLeft();
+						break;
 
-			case 38: // Up
-				person.positionX = person.positionX - person.velocity;
-				$("#player").offset({ top: person.positionX, left: person.positionY});
-				//$("#player").css('top', position.top - 20 + 'px');
-				//Game.player.moveUp();
-				break;
+					case 38: // Up
+						person.updateAnimationTime();
+						person.positionX = person.positionX - person.velocity;
+						$("#player").offset({ top: person.positionX, left: person.positionY});
+						//$("#player").css('top', position.top - 20 + 'px');
+						//Game.player.moveUp();
+						break;
 
-			case 39: // Right
-				person.positionY = person.positionY + person.velocity;
-				$("#player").offset({ top: person.positionX, left: person.positionY});
-				//$("#player").css('left', position.left + 20 + 'px');
-				//Game.player.moveRight();
-				break;
+					case 39: // Right
+						person.updateAnimationTime();
+						person.positionY = person.positionY + person.velocity;
+						$("#player").offset({ top: person.positionX, left: person.positionY});
+						//$("#player").css('left', position.left + 20 + 'px');
+						//Game.player.moveRight();
+						break;
 
-			case 40: // Down
-				person.positionX = person.positionX + person.velocity;
-				$("#player").offset({ top: person.positionX, left: person.positionY});
-				//$("#player").css('top', position.top + 20 + 'px');
-				//Game.player.moveDown();
-				break;
+					case 40: // Down
+						person.updateAnimationTime();
+						person.positionX = person.positionX + person.velocity;
+						$("#player").offset({ top: person.positionX, left: person.positionY});
+						//$("#player").css('top', position.top + 20 + 'px');
+						//Game.player.moveDown();
+						break;
+					case 32:
+						person.typeAction=1;
+						break;
+				}			
+				
 			}
 			
 		});	
